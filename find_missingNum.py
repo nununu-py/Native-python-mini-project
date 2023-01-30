af8c9251d23bf32ed69f3ba50cbe9690a9ee2fe9
nununu-py <09021381621081@student.unsri.ac.id>
2023-01-30 23:48:33 +0700

    create find_missingNum.py

---
+++ find_missingNum.py
@@ -0,0 +1,15 @@
+def missing_num(number):
+
+    miss_val = []
+    min_num = min(number)
+    max_num = max(number)
+   
+    for i in range(min_num, max_num):
+        if i not in number:
+            miss_val.append(i)
+
+    return miss_val
+
+
+number = [2, 4, 4, 5, 4, 6, 7, 8, 10, 16, 17, 18, 20]
+print(missing_num(number))
